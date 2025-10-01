34fe11f722eb66b53e55ff48c3c81d1d27a4783b
Artem Veremienko <temaspsxxx@gmail.com>
2025-10-02 00:14:06 +0300

    feat: project scaffolding

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export const SocialPlatform = {
+  TIKTOK: 'TikTok',
+  INSTAGRAM: 'Instagram',
+  YOUTUBE: 'YouTube',
+} as const;
+
+export const PublicationStatus = {
+  DRAFT: 'Draft',
+  SCHEDULED: 'Scheduled',
+  PUBLISHED: 'Published',
+  ERROR: 'Error',
+} as const;

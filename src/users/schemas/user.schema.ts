bdeaf5b6433c512e1433788df88b18bf5efd9490
Artem Veremienko <temaspsxxx@gmail.com>
2025-10-07 20:54:40 +0300

    feat: rename user to users module

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -3,11 +3,11 @@ import { AccountSchema, Account } from './account.schema';
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true, trim: true })
-  name: string;
+  @Prop({ required: true, trim: true, unique: true })
+  username: string;
 
-  @Prop({ required: true, unique: true, lowercase: true })
-  email: string;
+  @Prop({ required: true })
+  password: string;
 
   @Prop({ type: [AccountSchema], default: [] })
   connectedAccounts: Account[];
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
 
 @Injectable()
-export class UserService {
-  constructor(
-    @InjectModel(User.name) private readonly userModel: Model<User>,
-  ) {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
 
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(username: string) {
+    return this.users.find((user) => user.username === username);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {

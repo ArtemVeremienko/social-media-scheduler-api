ea51dbeeb049ff9888fd12d703a332e5411f338d
Artem Veremienko <temaspsxxx@gmail.com>
2025-10-07 20:55:57 +0300

    feat: add auth guard and strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { APP_GUARD } from '@nestjs/core';
+import { BasicAuthGuard } from './guards/basic.guard';
+import { BasicStrategy } from './strategies/basic.strategy';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    BasicStrategy,
+    { provide: APP_GUARD, useClass: BasicAuthGuard }, // TODO: change to JwtAuthGuard
+  ],
+  exports: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/strategies/basic.strategy.ts
@@ -0,0 +1,22 @@
+import { BasicStrategy as NativeBasicStrategy } from 'passport-http';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class BasicStrategy extends PassportStrategy(
+  NativeBasicStrategy,
+  'basic',
+) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}

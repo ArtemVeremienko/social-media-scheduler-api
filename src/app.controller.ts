0f6ff0085f9cb01c13063e606f5fb9ce4eaaabba
Artem Veremienko <temaspsxxx@gmail.com>
2025-10-07 21:02:28 +0300

    feat: add test endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
